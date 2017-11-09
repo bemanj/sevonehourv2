d612a933e8a1f99f750fe99902d77c11f199a006
bemanj <30899182+bemanj@users.noreply.github.com>
2017-11-10 01:32:47 +0800

    Update temp-data.ts

--- src/app/shared/models/temp-data.ts
+++ src/app/shared/models/temp-data.ts
@@ -2,10 +2,12 @@ export class TempWallBoardData {
    public _id: number;
    public _incidentNumber: string;
    public _remainingSeconds: number;
+   public _startDate: Date;
 
-    constructor(id: number, incidentNumber: string, remainingSeconds: number) {
+    constructor(id: number, incidentNumber: string, remainingSeconds: number, startDate: Date) {
         this._id = id;
         this._incidentNumber = incidentNumber;
         this._remainingSeconds = remainingSeconds;
+        this._startDate = startDate;
     }
 }
